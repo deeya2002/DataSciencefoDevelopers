aee1e82e4e1946d8c24088c52db9922e2a7efee6
deeya2002 <diyadangol2002@gmail.com>
2023-08-17 00:08:46 +0545

    add code

---
+++ Rcode/BroadBand.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(dplyr)
+library(stringi)
+library(scales)
+
+setwd("D:/Diy_DataAssign/")
+
+# BROADBAND DATA CLEANING
+Broadband = read_csv("Datasets/Broadband_Speed/201805_fixed_pc_performance_r03.csv", show_col_types = FALSE)
+
+
+pattern = ' .*$'
+BroadbandData = Broadband %>%
+  mutate(shortPostcode=gsub(pattern,"",postcode_space)) %>% 
+  mutate(ID = row_number()) %>% 
+  select(`ID`, `postcode area`, shortPostcode, `Average download speed (Mbit/s)`,
+         `Average upload speed (Mbit/s)`, `Minimum download speed (Mbit/s)`,
+         `Minimum upload speed (Mbit/s)`) %>% 
+  na.omit()
+colnames(BroadbandData) = c( "ID","postcode area", "shortPostcode", "Avgdownload",
+                             "Average upload speed (Mbit/s)", "Mindownload",
+                             "Minimum upload speed (Mbit/s)")
+write.csv(BroadbandData, "Clean/Cleaned_Broadband_Speed.csv",row.names = FALSE)
+
+
+
